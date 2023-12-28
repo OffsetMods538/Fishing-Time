74fbaea334468130b44424d237a6d1652f856bc8
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-12-28 12:09:52 +0200

    Calculate the fishing time when a fish is caught in seconds. Then show it to the user.

--- src/client/java/top/offsetmonkey538/fishingtime/mixin/client/FishingBobberEntityMixin.java
+++ src/client/java/top/offsetmonkey538/fishingtime/mixin/client/FishingBobberEntityMixin.java
@@ -1,10 +1,13 @@
 package top.offsetmonkey538.fishingtime.mixin.client;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +18,9 @@ public abstract class FishingBobberEntityMixin extends Entity {
     public FishingBobberEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
-    
+
+    @Shadow private boolean caughtFish;
+
     @Unique
     private long fishingTime$fishingStartTime;
     
@@ -28,5 +33,21 @@ public abstract class FishingBobberEntityMixin extends Entity {
     )
     private void storeFishingStartTime(CallbackInfo ci) {
         fishingTime$fishingStartTime = System.nanoTime();
+    }
+
+    @Inject(
+            method = "tick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/Math;max(II)I"
+            )
+    )
+    private void finishMeasuringFishingTime(CallbackInfo ci) {
+        if (!caughtFish || fishingTime$fishingStartTime == 0) return;
+
+        final double fishingTimeSeconds = (double) (System.nanoTime() - fishingTime$fishingStartTime) / 1_000_000_000;
+        fishingTime$fishingStartTime = 0;
+
+        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("Fishing finished in " + fishingTimeSeconds + " seconds."));
     }
 }
