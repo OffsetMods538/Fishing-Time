ec0dea93b622eb3430d35c543536b172c09f5ee2
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-12-28 10:59:26 +0200

    Initial Commit

---
+++ src/client/java/top/offsetmonkey538/fishingtime/FishingTimeClient.java
@@ -0,0 +1,11 @@
+package top.offsetmonkey538.fishingtime;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishingTimeClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// Do stuff
+	}
+}
---
+++ src/main/java/top/offsetmonkey538/fishingtime/FishingTime.java
@@ -0,0 +1,20 @@
+package top.offsetmonkey538.fishingtime;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FishingTime implements ModInitializer {
+	public static final String MOD_ID = "fishing-time";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// Do stuff
+	}
+
+	public static Identifier id(String path) {
+		return new Identifier(MOD_ID, path);
+	}
+}
